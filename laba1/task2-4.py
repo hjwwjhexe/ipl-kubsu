253ff586c456ec1650d3c4c62a4c526c5c6b3233
Владимир Артамонов <hjwwjh36@gmail.com>
2025-09-27 00:10:15 +0300

    даты формата «день.месяц.год»

--- laba1/task2-4.py
+++ laba1/task2-4.py
@@ -1,3 +1,5 @@
+import re#guliarki fu
+
 def count_russian_chars(s):
     russian_letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     count = 0
@@ -10,7 +12,12 @@ def is_lower_latin_palindrome(s):
     latin_lower = [ch for ch in s if ch.isalpha() and ch.islower() and 'a' <= ch <= 'z']
     return latin_lower == latin_lower[::-1]
 
+def find_dates(text):
+    pattern = r'\b(0?[1-9]|[12]\d|3[01])\.(0?[1-9]|1[0-2])\.([12]\d{3})\b'
+    return re.findall(pattern, text)
+
 if __name__ == "__main__":
     text = input("Введите строку: ")
     print("Количество русских символов:", count_russian_chars(text))
     print("Строчные латинские символы образуют палиндром?", is_lower_latin_palindrome(text))
+    print("Найденные даты:", find_dates(text))
