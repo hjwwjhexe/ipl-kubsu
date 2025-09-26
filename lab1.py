2427157eab2c470c7d0a8f32880004d996fffffd
Владимир Артамонов <hjwwjh36@gmail.com>
2025-09-26 23:30:09 +0300

    Произведение делителей, у которых сумма цифр меньше суммы цифр исходного числа

--- lab1.py
+++ lab1.py
@@ -22,7 +22,22 @@ def count_odd_digits_gt_3(num):
                 count += 1
     return count
 
+def sum_of_digits(n):
+    return sum(int(d) for d in str(n))
+
+def product_special_divisors(num):
+    total_sum = sum_of_digits(num)
+    product = 1
+    found = False
+    for i in range(1, num + 1):
+        if num % i == 0:
+            if sum_of_digits(i) < total_sum:
+                product *= i
+                found = True
+    return product if found else 0
+
 if __name__ == "__main__":
     n = int(input("Введите число: "))
     print("1. Сумма простых делителей:", sum_prime_divisors(n))
     print("2. Количество нечетных цифр > 3:", count_odd_digits_gt_3(n))
+    print("3. Произведение делителей с суммой цифр меньше исходной:", product_special_divisors(n))
