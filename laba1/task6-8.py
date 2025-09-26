41075fe2cd392771173419b5aa4702502c11fb3d
Владимир Артамонов <hjwwjh36@gmail.com>
2025-09-27 00:19:43 +0300

    Наибольшее количество идущих подряд цифр

--- laba1/task6-8.py
+++ laba1/task6-8.py
@@ -12,7 +12,14 @@ def min_rational_in_string(s):
         return None
     return min(map(float, numbers))
 
+def max_consecutive_digits(s):
+    digits_sequences = re.findall(r'\d+', s)
+    if not digits_sequences:
+        return 0
+    return max(len(seq) for seq in digits_sequences)
+
 if __name__ == "__main__":
     text = input("Введите строку: ")
     print("Максимальное вещественное число:", max_float_in_string(text))
     print("Минимальное рациональное число:", min_rational_in_string(text))
+    print("Наибольшее количество идущих подряд цифр:", max_consecutive_digits(text))
