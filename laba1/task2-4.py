1b09d1a2a7e296cc0a570552629118022dfc0eae
Владимир Артамонов <hjwwjh36@gmail.com>
2025-09-26 23:38:14 +0300

    Строчные латинские символы образуют палиндром(или нет)

--- laba1/task2-4.py
+++ laba1/task2-4.py
@@ -6,6 +6,11 @@ def count_russian_chars(s):
             count += 1
     return count
 
+def is_lower_latin_palindrome(s):
+    latin_lower = [ch for ch in s if ch.isalpha() and ch.islower() and 'a' <= ch <= 'z']
+    return latin_lower == latin_lower[::-1]
+
 if __name__ == "__main__":
     text = input("Введите строку: ")
     print("Количество русских символов:", count_russian_chars(text))
+    print("Строчные латинские символы образуют палиндром?", is_lower_latin_palindrome(text))
