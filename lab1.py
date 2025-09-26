1e443667cec23997524574c01bebd1e6f56ef0a6
Владимир Артамонов <hjwwjh36@gmail.com>
2025-09-26 23:28:41 +0300

    Количество нечетных цифр числа, больших 3

--- lab1.py
+++ lab1.py
@@ -13,6 +13,16 @@ def sum_prime_divisors(num):
             total += i
     return total
 
+def count_odd_digits_gt_3(num):
+    count = 0
+    for digit in str(num):
+        if digit.isdigit():
+            d = int(digit)
+            if d % 2 == 1 and d > 3:
+                count += 1
+    return count
+
 if __name__ == "__main__":
     n = int(input("Введите число: "))
-    print("Сумма простых делителей:", sum_prime_divisors(n))
+    print("1. Сумма простых делителей:", sum_prime_divisors(n))
+    print("2. Количество нечетных цифр > 3:", count_odd_digits_gt_3(n))
